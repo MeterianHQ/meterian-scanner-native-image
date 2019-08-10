9e9618d398151973f519fec7bdd9429950235168
mani <sadhak001@gmail.com>
2019-08-10 17:15:25 +0100

    Updating reflection config json file with more configurations for the build time

--- runGraalVMinDocker.sh
+++ runGraalVMinDocker.sh
@@ -8,6 +8,7 @@ docker run -it                                            \
            --volume $(pwd):/workspace                     \
            --volume $HOME/.meterian:/root/.meterian       \
            --env METERIAN_API_TOKEN=${METERIAN_API_TOKEN} \
+           --env JAVA_HOME=/graalvm                       \
            --workdir /workspace                           \
            findepi/graalvm:19.1.1-all                     \
            /bin/bash
--- runPlainJavainDocker.sh
+++ runPlainJavainDocker.sh
@@ -8,6 +8,7 @@ docker run -it                                            \
            --volume $(pwd):/workspace                     \
            --volume $HOME/.meterian:/root/.meterian       \
            --env METERIAN_API_TOKEN=${METERIAN_API_TOKEN} \
+           --env JAVA_HOME=/opt/java/openjdk              \
            --workdir /workspace                           \
            adoptopenjdk/openjdk8:jdk8u212-b03             \
            /bin/bash
